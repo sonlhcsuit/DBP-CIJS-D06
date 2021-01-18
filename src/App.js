4b64dc8cad7723f37c92b741330674bb066a03ee
Son Luu <sonlh.cs.uit@gmail.com>
2021-01-18 21:52:13 +0700

    firebase func

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { signUp, addTodo, editTodo, getTodo } from './ultis/ultis'
-// signUp({ username: 'thoth', password: 'cuncon' })
-//   .then(userId => {
-//     console.log(userId)
-//   })
-//   .catch(er => {
-//     // console.error(er.stack)
-//   })
 
-// editTodo({
-//   todoId:'PHjMLNjVhwqA5mth7REw',
-//   ownerId: '552aSmFpE2WOZpCIVKM5',
-//   title: 'Everyday working 123123',
-//   content: 'di cho, nau com,hoc lap trinh, dau tu bds ...'
-// }).then(data=>{
-//   console.log(data)
-// })
-// getTodo('552aSmFpE2WOZpCIVKM5').then(todos=>{
-//   console.log(todos)
-// })
 
 
 function App() {
